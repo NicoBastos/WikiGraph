36ae6558493c7a96abfbd7bdfec6dc7f2e00fff4
nickbastos <nickbastos4@gmail.com>
2024-01-27 16:45:27 -0600

    vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,12 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig((command) => {
-  const config = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "./src"),
-      },
-      base: "",
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
-  };
-
-  if (command !== "serve") {
-    config.base = "/react-vite-gh-pages/";
-  }
-
-  return config;
+  },
+  base: "https://nicobastos.github.io/WikiGraph/",
 });
