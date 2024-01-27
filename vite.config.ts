65c6932d52c175f40445fb155c815aaa6192cf54
nickbastos <nickbastos4@gmail.com>
2024-01-27 16:41:11 -0600

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,20 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig((command) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+      base: "",
     },
-  },
+  };
+
+  if (command !== "serve") {
+    config.base = "/react-vite-gh-pages/";
+  }
+
+  return config;
 });
